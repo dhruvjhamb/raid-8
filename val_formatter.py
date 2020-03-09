0a22e068a9ba4b5d736a42b6726e73811d68c64c
Akshat Gokhale <ajgokhale@gmail.com>
2020-03-09 14:52:47 -0400

    Generalized val_formatter for all setup (i.e. create checkpoints dir)

--- val_formatter.py
+++ val_formatter.py
@@ -8,6 +8,11 @@ from model import Net
 import shutil
 import os
 
+def create_ckpts_dir():
+    checkpoint_dir = pathlib.Path('./checkpoints')
+    if not os.path.isdir(checkpoint_dir):
+        os.mkdir(checkpoint_dir)
+
 def main():
 	classes = dict()
 	data_dir = pathlib.Path('./data/tiny-imagenet-200/val/')
@@ -23,7 +28,8 @@ def main():
 				break
 			if classes.get(tokens[1]) == None:
 				classes[tokens[1]] = data_dir / tokens[1]
-				os.mkdir(classes[tokens[1]])
+                if not os.path.isdir(classes[tokens[1]]):
+				    os.mkdir(classes[tokens[1]])
 			shutil.copy(img_dir / tokens[0], classes[tokens[1]])
 			line = fp.readline()
 			cnt += 1
