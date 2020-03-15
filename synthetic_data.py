4743d35c46eefd83eb60cfa879f5b9a5908dc06e
Akshat Gokhale <ajgokhale@gmail.com>
2020-03-15 19:56:09 -0400

    Added data manipulation scripts

---
+++ synthetic_data.py
@@ -0,0 +1,90 @@
+"""
+This file will train a sample network on the tiny image-net data. It should be
+your final goal to improve on the performance of this model by swapping out large
+portions of the code. We provide this model in order to test the full pipeline,
+and to validate your own code submission.
+"""
+
+import pathlib
+import argparse
+import os
+
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from model import *
+import time
+
+from torch import nn
+
+def parse():
+    parser = argparse.ArgumentParser(description='Train or validate predefined models.')
+#    parser.add_argument('--val', action='store_true')
+#    parser.add_argument('--data', type=float, default=1.0)
+#    parser.add_argument('--checkpoint', type=str, default='latest.pt')
+#    parser.add_argument('models', metavar='model', type=str, nargs='*')
+    return parser.parse_args()
+
+def map_classes(dir_path):
+    classes = os.listdir(dir_path)
+    classes.sort()
+    idx_to_class = {i: classes[i] for i in range(len(classes))}
+    return idx_to_class
+
+def try_mkdir(dir_path):
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
+
+def main():
+    args = parse() 
+
+    # Create a pytorch dataset
+    data_dir = pathlib.Path('./data/tiny-imagenet-200')
+    image_count = len(list(data_dir.glob('**/*.JPEG')))
+    training_image_count = len(list(data_dir.glob('train/**/*.JPEG')))
+    CLASS_NAMES = np.array([item.name for item in (data_dir / 'train').glob('*')])
+    print('Discovered {} total images'.format(image_count))
+    print('Discovered {} training images'.format(training_image_count))
+
+    # Create the training data generator
+    im_height = 64
+    im_width = 64
+
+    data_transforms = dict()    
+
+    data_transforms['rotate'] = transforms.Compose([
+        transforms.RandomHorizontalFlip(p=1.0),
+        transforms.ToTensor(),
+    ])
+
+    class_map = map_classes(data_dir / 'train')
+    transform_dir = pathlib.Path('./data/tiny-imagenet-transformed/train/')
+    for transformation in data_transforms.keys():
+        curr_transform_dir = transform_dir / transformation
+        try_mkdir(curr_transform_dir)
+        data_transform = data_transforms[transformation]
+        train_set = torchvision.datasets.ImageFolder(data_dir / 'train', data_transform)
+        train_loader = torch.utils.data.DataLoader(train_set, batch_size=1,
+                                                   shuffle=False, num_workers=1, pin_memory=True)
+
+        curr_target = -1
+        for idx, (inputs, targets) in enumerate(train_loader):
+            target = targets[0].item()
+            if target != curr_target:
+                curr_target = target
+                class_counter = 0
+
+            class_name = class_map[target]
+            class_path = curr_transform_dir / class_name
+            try_mkdir(class_path)
+            images_path = class_path / 'images'
+            try_mkdir(images_path)
+
+            img_path = images_path / '{}_{}.JPEG'.format(class_name, class_counter)
+            torchvision.utils.save_image(inputs[0], img_path)
+
+            class_counter += 1
+
+if __name__ == '__main__':
+    main()
---
+++ train_formatter.py
@@ -0,0 +1,38 @@
+import sys
+import pathlib
+from PIL import Image
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from model import Net
+import shutil
+import os
+
+def try_mkdir(dir_path):
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
+
+def proper_order(path):
+    tokens = path.split('_')
+    tokens[1] = tokens[1].zfill(3)
+    return '_'.join(tokens)
+
+def main():
+    classes = dict()
+    data_dir = pathlib.Path('./data/tiny-imagenet-200')
+#    ordered_dir = data_dir / 'train_ordered'
+#    try_mkdir(ordered_dir)
+    training_images = list(data_dir.glob('train/**/*.JPEG'))
+
+    for idx, image in enumerate(training_images):
+        print("\r", end='')
+        print(f'Renamed {100 * idx / len(training_images):.2f}%', end='')
+#        class_dir = ordered_dir / image.parts[-3]
+#        try_mkdir(class_dir)
+#        img_dir = class_dir / image.parts[-2]
+#        try_mkdir(img_dir)
+        ordered_img = image.parent / (proper_order(image.stem) + image.suffix)
+        shutil.move(image, ordered_img)
+
+if __name__ == '__main__':
+	main()
