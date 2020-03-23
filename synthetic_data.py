398352df0ffa87eacc532b003234165177415176
Akshat Gokhale <ajgokhale@gmail.com>
2020-03-23 16:44:39 -0400

    Display generated samples for verification(WIP)

--- synthetic_data.py
+++ synthetic_data.py
@@ -13,18 +13,17 @@ import numpy as np
 import torch
 import torchvision
 import torchvision.transforms as transforms
-from model import *
-import time
-
-from torch import nn
+from PIL import Image
 
 from utils import *
 
 IMAGES_PER_CLASS = 500
+NUM_CLASSES = 200
 
 def parse():
     parser = argparse.ArgumentParser(description='Train or validate predefined models.')
     parser.add_argument('--overwrite', action='store_true')
+    parser.add_argument('--generate_samples', type=int, default=0)
     return parser.parse_args()
 
 def map_classes(dir_path):
@@ -37,14 +36,31 @@ def pairwise_equal(t):
     first_elem = torch.full(t.size(), t[0].item(), dtype=torch.long)
     return torch.equal(t, first_elem)
 
+def get_generation_indices(num_batches, transform_batch_size, num_generated_samples):
+    total_samples = num_batches * transform_batch_size
+    generate_arr = np.zeros(total_samples)
+    indices = np.random.choice(generate_arr.size, replace=False,
+                           size=num_generated_samples)
+    generate_arr[indices] = 1
+    return generate_arr.reshape((num_batches, transform_batch_size))
+
+def get_concat_h(im1_path, im2_path):
+    GAP = 20 
+    im1, im2 = Image.open(im1_path), Image.open(im2_path)
+    dst = Image.new('RGB', (im1.width + im2.width + GAP, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width + GAP, 0))
+    return dst
+
 def main():
     args = parse() 
 
     # Create a pytorch dataset
     data_dir = pathlib.Path('./data/tiny-imagenet-200')
+    source_dir = data_dir / 'train'
     image_count = len(list(data_dir.glob('**/*.JPEG')))
     training_image_count = len(list(data_dir.glob('train/**/*.JPEG')))
-    CLASS_NAMES = np.array([item.name for item in (data_dir / 'train').glob('*')])
+    CLASS_NAMES = np.array([item.name for item in source_dir.glob('*')])
     print('Discovered {} total images'.format(image_count))
     print('Discovered {} training images'.format(training_image_count))
 
@@ -64,10 +80,11 @@ def main():
         0.01
     )
 
-    class_map = map_classes(data_dir / 'train')
-    transform_dir = pathlib.Path('./data/tiny-imagenet-transformed/train/')
+    class_map = map_classes(source_dir)
+    transform_dir = pathlib.Path('./data/tiny-imagenet-transformed') / 'train'
 
     batch_size = IMAGES_PER_CLASS
+    num_batches = NUM_CLASSES
     for transformation in data_transforms.keys():
         curr_transform_dir = transform_dir / transformation
         if args.overwrite:
@@ -77,12 +94,18 @@ def main():
 
         # Read in and transform all images
         data_transform, sampling_rate = data_transforms[transformation]
-        train_set = torchvision.datasets.ImageFolder(data_dir / 'train', data_transform)
+        num_generated_samples = args.generate_samples
+
+        train_set = torchvision.datasets.ImageFolder(source_dir, data_transform)
         train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
                                                    shuffle=False, num_workers=1, pin_memory=True)
 
         curr_target = -1
         num_permutations = int(sampling_rate * batch_size)
+
+        generation_indices = get_generation_indices(num_batches, num_permutations,
+            num_generated_samples)        
+
         for idx, (inputs, targets) in enumerate(train_loader):
             assert(pairwise_equal(targets))
             target = targets[0].item()
@@ -93,13 +116,19 @@ def main():
             images_path = class_path / 'images'
             try_mkdir(images_path)
 
+            source_path = source_dir / class_name / 'images'
+
             # Randomly sample images to permute
             indices = torch.randperm(batch_size)
             
-            for index in indices[:num_permutations]:
-                img_path = images_path / '{}_{}.JPEG'.format(class_name,
+            for gen_idx, index in enumerate(indices[:num_permutations]):
+                img_name = '{}_{}.JPEG'.format(class_name,
                     proper_order_int(str(index.item())))
+                img_path = images_path / img_name
                 torchvision.utils.save_image(inputs[index.item()], img_path)
+                if generation_indices[idx][gen_idx] == 1:    
+                    # Concatenate source and transformed image, and display the result
+                    get_concat_h(source_path / img_name, img_path).show()
 
 if __name__ == '__main__':
     main()
