dea3229090b10976c89a62da6ce0e133e5f0c129
Rahul Hayaran <rahul.hayaran@berkeley.edu>
2020-04-14 19:38:09 -0700

    added rotation and perspective transforms

--- transform.py
+++ transform.py
@@ -19,3 +19,24 @@ data_transforms['flip'] = (transforms.Compose([
     ]),
     0.01
 )
+
+data_transforms['rotate10to15'] = (transforms.Compose([
+    transforms.RandomRotation(degrees=(10,15), resample=Image.BICUBIC),
+    transforms.ToTensor(),
+    ]),
+    0.01
+)
+
+data_transforms['rotate20to30'] = (transforms.Compose([
+    transforms.RandomRotation(degrees=(20,30), resample=Image.BICUBIC),
+    transforms.ToTensor(),
+    ]),
+    0.01
+)
+
+data_transforms['perspective'] = (transforms.Compose([
+    transforms.RandomPerspective(p=1.0),
+    transforms.ToTensor(),
+    ]),
+    0.01
+)
