2b8a1fd3bf815a7d98ac5a36eb2b6a9f38e23fb1
Akshat Gokhale <ajgokhale@gmail.com>
2020-05-10 19:08:31 -0700

    Experiments (wip)

--- experiment.sh
+++ experiment.sh
@@ -11,22 +11,19 @@
 # CUDA_VISIBLE_DEVICES=1 python train.py --data 15 resnext --learningrates 0 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.75 --logfile decay-6_3_1-75
 # CUDA_VISIBLE_DEVICES=1 python train.py --data 15 resnext --learningrates 0 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.5 --logfile  decay-6_3_1-50 
 # CUDA_VISIBLE_DEVICES=1 python train.py --data 15 resnext --learningrates 0 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.9 --logfile  decay-6_3_1-90
-# CUDA_VISIBLE_DEVICES=1 python train.py --data 15 resnext --learningrates 0 1e-4 1e-3 --partitions 6 2 2 --decayrate 1 --decaycoeff 0.75 --logfile decay-6_2_2-75
-# CUDA_VISIBLE_DEVICES=1 python train.py --data 15 resnext --learningrates 0 1e-4 1e-3 --partitions 6 2 2 --decayrate 1 --decaycoeff 0.5 --logfile  decay-6_2_2-50 
-# CUDA_VISIBLE_DEVICES=1 python train.py --data 15 resnext --learningrates 0 1e-4 1e-3 --partitions 6 2 2 --decayrate 1 --decaycoeff 0.9 --logfile  decay-6_2_2-90
 
 # Unfreezing batchnorm layers 
 # CUDA_VISIBLE_DEVICES=0 python train.py --data 15 resnext --batchnorm_lr 1e-5 --learningrates 0 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.75 --logfile bn-6_3_1-1e5
 # CUDA_VISIBLE_DEVICES=0 python train.py --data 15 resnext --batchnorm_lr 1e-4 --learningrates 0 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.75 --logfile bn-6_3_1-1e4
 # CUDA_VISIBLE_DEVICES=0 python train.py --data 15 resnext --batchnorm_lr 1e-3 --learningrates 0 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.75 --logfile bn-6_3_1-1e3
-# CUDA_VISIBLE_DEVICES=0 python train.py --data 15 resnext --batchnorm_lr 1e-5 --learningrates 0 1e-4 1e-3 --partitions 6 2 2 --decayrate 1 --decaycoeff 0.75 --logfile bn-6_2_2-1e5
-# CUDA_VISIBLE_DEVICES=0 python train.py --data 15 resnext --batchnorm_lr 1e-4 --learningrates 0 1e-4 1e-3 --partitions 6 2 2 --decayrate 1 --decaycoeff 0.75 --logfile bn-6_2_2-1e4
-# CUDA_VISIBLE_DEVICES=0 python train.py --data 15 resnext --batchnorm_lr 1e-3 --learningrates 0 1e-4 1e-3 --partitions 6 2 2 --decayrate 1 --decaycoeff 0.75 --logfile bn-6_2_2-1e3
 
 # Train all layers 
-CUDA_VISIBLE_DEVICES=0 python train.py --data 20 resnext --learningrates 1e-5 1e-4 1e-3 --partitions 6 2 2 --decayrate 1 --decaycoeff 0.75 --logfile all-layers-1e5
-CUDA_VISIBLE_DEVICES=0 python train.py --data 20 resnext --learningrates 1e-6 1e-4 1e-3 --partitions 6 2 2 --decayrate 1 --decaycoeff 0.75 --logfile all-layers-1e6
+CUDA_VISIBLE_DEVICES=0 python train.py --data 20 resnext --learningrates 1e-5 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.75 --logfile all-layers-1e5
+CUDA_VISIBLE_DEVICES=0 python train.py --data 20 resnext --learningrates 1e-6 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.75 --logfile all-layers-1e6
+CUDA_VISIBLE_DEVICES=0 python train.py --data 20 resnext --learningrates 1e-5 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.5 --logfile all-layers-1e5-decay-50
 
 # Circular learning rates
-CUDA_VISIBLE_DEVICES=1 python train.py --data 20 resnext --learningrates 1e-5 1e-4 1e-3 --partitions 6 2 2 --decayrate 1 --decaycoeff 0.75 --circular_lr 0.5 --logfile circular-50
-CUDA_VISIBLE_DEVICES=1 python train.py --data 20 resnext --learningrates 1e-5 1e-4 1e-3 --partitions 6 2 2 --decayrate 1 --decaycoeff 0.75 --circular_lr 0.1 --logfile circular-50
+CUDA_VISIBLE_DEVICES=1 python train.py --data 20 resnext --learningrates 1e-5 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.75 --circular_lr 0.5 --logfile circular-50
+CUDA_VISIBLE_DEVICES=1 python train.py --data 20 resnext --learningrates 1e-5 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.75 --circular_lr 0.1 --logfile circular-10
+CUDA_VISIBLE_DEVICES=1 python train.py --data 20 resnext --learningrates 1e-5 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.5 --circular_lr 0.5 --logfile circular-50-decay-50
+CUDA_VISIBLE_DEVICES=1 python train.py --data 20 resnext --learningrates 1e-5 1e-4 1e-3 --partitions 6 3 1 --decayrate 1 --decaycoeff 0.5 --circular_lr 0.1 --logfile circular-10-decay-50
