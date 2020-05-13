b1deac07fe98c9a7e059d8a65f2815ee9f7f4da1
ishanv1 <ishan.vachhani@gmail.com>
2020-05-12 21:33:32 -0400

    submission verification files

---
+++ verify_test.py
@@ -0,0 +1,19 @@
+import pathlib
+import re
+
+results = []
+for line in pathlib.Path('./eval_classified.csv').open():
+	results.append(line.split(',')[1][:-1])
+
+
+correct = 0
+total = 0
+i = 0
+for line in pathlib.Path('./test_eval.csv').open():
+	actual = line.split(',')[1]
+	actual = re.search(r'n[0-9]+', actual).group(0)
+	if results[i] == actual:
+		correct += 1
+	total += 1
+
+print(correct/total)
